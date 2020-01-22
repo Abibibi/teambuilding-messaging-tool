2c97da4cc928905d8ae2ae48c9099bef8e2f01f8
Abibibi <abeba.ngwe@gmail.com>
2020-01-22 01:23:28 +0100

    login done with backend API

--- front-end/src/store/actions.js
+++ front-end/src/store/actions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import router from '@/router'
 
 export default {
   registering: ({ commit }, userInfo) => {
@@ -12,6 +13,21 @@ export default {
           commit('registerFailed')
         }
       })
-  }
+  },
 
+  login: ({ commit }, userInfo) => {
+    axios.post('http://localhost:5000/users/login', userInfo, { withCredentials: true })
+      .then((response) => {
+        console.log(response)
+        commit('loginDone', response.data)
+        router.push('mes-espaces')
+      })
+      .catch((err) => {
+        if (err.response.data.nonExistingUser) {
+          commit('loginNonExistingUser')
+        } else if (err.response.data.wrongPassword) {
+          commit('loginWrongPassword')
+        }
+      })
+  }
 }
--- front-end/src/store/mutations.js
+++ front-end/src/store/mutations.js
@@ -7,5 +7,32 @@ export default {
   registerFailed: (state) => {
     state.registerFail = true
     state.registerSuccess = false
+  },
+
+  loginDone: (state, payload) => {
+    state.loginSuccess = true
+    state.nonExistingUser = false
+    state.wrongPassword = false
+    state.logged = true
+    state.firstname = payload.firstname
+    state.email = payload.email
+  },
+
+  loginNonExistingUser: (state) => {
+    state.loginSuccess = false
+    state.nonExistingUser = true
+    state.wrongPassword = false
+    state.logged = false
+    state.firstname = ''
+    state.email = ''
+  },
+
+  loginWrongPassword: (state) => {
+    state.loginSuccess = false
+    state.nonExistingUser = false
+    state.wrongPassword = true
+    state.logged = false
+    state.firstname = ''
+    state.email = ''
   }
 }
